5a542f59d84b5782ee0cb5d9692c6a67c7720637
Andrew2060 <2060andrew@gmail.com>
2014-02-28 10:21:38 -0800

    Setting stuff up

---
+++ src/main/java/net/kingdomsofarden/crafty/Crafty.java
@@ -0,0 +1,13 @@
+package net.kingdomsofarden.crafty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Crafty extends JavaPlugin {
+    
+    
+    @Override
+    public void onEnable() {
+        
+    }
+
+}
---
+++ src/main/java/net/kingdomsofarden/crafty/api/items/CraftyItem.java
@@ -0,0 +1,11 @@
+package net.kingdomsofarden.crafty.api.items;
+
+import org.bukkit.inventory.meta.ItemMeta;
+
+public abstract class CraftyItem {
+    
+    public CraftyItem() {
+    }
+
+    
+}
