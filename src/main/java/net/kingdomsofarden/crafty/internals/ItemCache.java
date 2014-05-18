975437478eaed4b1ea72e684c84bd8473523668f
Andrew2060 <2060andrew@gmail.com>
2014-05-18 15:03:53 -0700

    Add debug code

--- src/main/java/net/kingdomsofarden/crafty/internals/ItemCache.java
+++ src/main/java/net/kingdomsofarden/crafty/internals/ItemCache.java
@@ -6,6 +6,8 @@ import java.util.concurrent.TimeUnit;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 
 import net.kingdomsofarden.crafty.Crafty;
 import net.kingdomsofarden.crafty.api.CraftyItem;
@@ -13,6 +15,15 @@ import net.kingdomsofarden.crafty.api.CraftyItem;
 
 public class ItemCache {
     
+    public class CacheRemovalListener implements RemovalListener<CacheKey, CraftyItem> {
+
+        @Override
+        public void onRemoval(RemovalNotification<CacheKey, CraftyItem> removed) {
+            removed.getValue().updateItem();
+        }
+
+    }
+
     private Cache<CacheKey, CraftyItem> cache;
     private Crafty plugin;
     
@@ -23,10 +34,12 @@ public class ItemCache {
                 .softValues()
                 .weakKeys()
                 .expireAfterAccess(15,TimeUnit.MINUTES)
+                .removalListener(new CacheRemovalListener())
                 .build(new CacheLoader<CacheKey, CraftyItem>() {
 
                     @Override
                     public CraftyItem load(CacheKey obj) {
+                        System.out.println("Debug: Cache Miss");
                         return new CraftyItem(obj, plugin); 
                     }
                 });
