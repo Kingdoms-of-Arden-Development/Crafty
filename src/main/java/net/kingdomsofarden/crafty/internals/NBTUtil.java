96b5f42821b8af05add19ff33e385c4739658581
Andrew2060 <2060andrew@gmail.com>
2014-09-21 18:36:27 -0700

    Allow empty strings but not null

--- src/main/java/net/kingdomsofarden/crafty/internals/NBTUtil.java
+++ src/main/java/net/kingdomsofarden/crafty/internals/NBTUtil.java
@@ -71,8 +71,8 @@ public class NBTUtil {
      * @param item
      */
     public static void writeData(UUID id, String data, ItemStack item) {
-        if (data == null || data.equals("")) {
-            throw new IllegalArgumentException("Stored data is null or empty for module id " + id);
+        if (data == null) {
+            throw new IllegalArgumentException("Stored data is null for module id " + id);
         }
         AttributeStorage storage = AttributeStorage.newTarget(item, id);
         storage.setData(data);
